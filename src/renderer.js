6eee4b67fd70caca1706bf5ed1bd7e4a59cbac55
Noah Kiser <noah.kiser@gmail.com>
2024-12-23 11:52:03 -0600

    Updates

--- src/renderer.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from "three";
-export const renderer = new THREE.WebGLRenderer({
-  alpha: true,
-  antialias: true,
-});
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setSize(window.innerWidth, window.innerHeight);
