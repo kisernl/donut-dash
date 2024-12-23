359c04ef193a4f4f43d3d7ca3a24602c1902d11e
Noah Kiser <noah.kiser@gmail.com>
2024-12-23 11:42:04 -0600

    rebuild with src in public

---
+++ dist/src/constants.js
@@ -0,0 +1,11 @@
+export const zoom = 3;
+export const characterSize = 15;
+export const positionWidth = 42;
+export const columns = 17;
+export const boardWidth = positionWidth * columns;
+export const stepTime = 200;
+
+export const laneTypes = ["car", "truck", "forest"];
+export const laneSpeeds = [2, 2.5, 3];
+export const vehicleColors = [0xa52523, 0xbdb638, 0x78b14b];
+export const treeHeights = [20, 45, 60];
